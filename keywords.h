a41caac49ab9eb056a324da1196aa651d269f8c8
zlx <zlx20010815@gmail.com>
2026-04-22 09:23:50 +0100

    new file:   keywords.h

---
+++ keywords.h
@@ -0,0 +1,5 @@
+char *keywords[] = {"LET", "PRINT", "INPUT", "IF", "THEN", "FOR", "TO",
+                  	 "STEP", "NEXT", "GOTO", "GOSUB", "RETURN", "END",
+                  	 "REM", "AND", "OR", "NOT"};
+
+#define KEYWORDS_SIZE sizeof(keywords) / sizeof(keywords[0])
